0810a556bf4a8ecf605985102e7e50a62fb213cd
zhanghui <huizhang1995@gmail.com>
2018-03-29 17:38:04 +0800

    A Pythonic Card Deck

---
+++ Chapter1_ThePythonDataModel/APythonicCardDeck.py
@@ -0,0 +1,16 @@
+import collections
+
+Card = collections.namedtuple("Card", ['rank', 'suit'])
+
+class FrenchDeck(object):
+    ranks = [str(n) for n in range(2, 11)] + list("JOKA")
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for rank in self.ranks for suit in self.suits]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
